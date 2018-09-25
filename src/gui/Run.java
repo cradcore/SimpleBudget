daa7d16567d6a0e1a83c35b476e1913be9c6d0ff
Corey <corey.craddock@gmail.com>
2018-09-24 21:33:47 -0600

    Removed unused budgets from the database on close

--- src/gui/Run.java
+++ src/gui/Run.java
@@ -4,6 +4,9 @@ import net.miginfocom.swing.MigLayout;
 import sqlConnector.SQLConnector;
 
 import java.awt.EventQueue;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.ResultSet;
 import javax.swing.*;
 
 public class Run {
@@ -16,14 +19,35 @@ public class Run {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setContentPane(new JPanel(new MigLayout("gap rel 0", "grow")));
         window.getContentPane().setName("Home content pane");
+        window.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                removeUnusedBudgetMonths(sql);
+            }
+        });
 
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
                     new Budget(window, sql);
-                } catch (Exception e) {e.printStackTrace();}
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
+    }
+
+    private static void removeUnusedBudgetMonths(SQLConnector sql) {
+        ResultSet rs = sql.select("SELECT * FROM MonthBudget m WHERE NOT EXISTS(SELECT * FROM Entry e WHERE m.dateYear = e.dateYear AND m.dateMonth = e.dateMonth)");
+        try {
+            while (rs.next()) {
+                String catID = rs.getString("catID");
+                new SQLConnector().update("DELETE FROM `simpleBudget`.`MonthBudget` WHERE `catID` LIKE '" + catID + "' ESCAPE '#'");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
